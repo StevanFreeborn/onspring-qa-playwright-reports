f5b70d9f9b63726a93c3f9547f56013c352418b1
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-11-16 09:53:50 -0600

    fix: report date display
    + server report dates initially displayed in UTC
    + when page is loaded on client side we grab the values
      and convert them to the client's local time

---
+++ public/js/index.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+  convertDatesFromUtcToLocal();
+
+  /**
+   * @summary Converts all report dates from server rendered UTC values to local time
+   * @returns {void}
+   */
+  function convertDatesFromUtcToLocal() {
+    const dates = [...document.querySelectorAll('.report-date')];
+    for (const date of dates) {
+      date.textContent = 'Converting to local time...';
+      const dateInMs = parseInt(date.dataset.dateInMs);
+      const localDateString = new Date(parseInt(dateInMs)).toLocaleString();
+      date.textContent = localDateString;
+    }
+  }
+});

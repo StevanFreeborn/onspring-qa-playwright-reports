3acee506aafb9b48197c8a3d66b619a0dc1c136e
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-11-15 21:59:03 -0600

    feat: add responsive nav

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('logout').addEventListener('click', logOutUser);
+  document.getElementById('toggle').addEventListener('click', toggleNav);
 
   const csrfToken = document
     .querySelector('meta[name="csrf-token"]')
@@ -23,5 +24,13 @@ document.addEventListener('DOMContentLoaded', () => {
     if (response.redirected) {
       window.location.href = response.url;
     }
+  }
+
+  /**
+   * @summary Toggle the navigation menu
+   */
+  function toggleNav() {
+    const nav = document.querySelector('.right-header');
+    nav.classList.toggle('show');
   }
 });

52e681ada8c7e9aa8a314971afee831e0c181e37
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-11-16 11:21:33 -0600

    fix: only require user email to register new user

--- services/users.js
+++ services/users.js
@@ -10,16 +10,10 @@ import { Result } from '../utils/result.js';
  * @param {object} params - The options to use.
  * @param {string} params.email - The user's email address.
  * @param {string} params.password - The user's password.
- * @param {string} params.verifyPassword - The user's verified password.
  * @param {PrismaClient} [params.client] - The Prisma client to use.
  * @returns {Promise<Result>} The result of the operation.
  */
-export async function registerUser({
-  email,
-  password,
-  verifyPassword,
-  client = prismaClient,
-}) {
+export async function registerUser({ email, password, client = prismaClient }) {
   const user = await client.user.findUnique({
     where: {
       email,
@@ -28,10 +22,6 @@ export async function registerUser({
 
   if (user !== null) {
     return Result.failure(new Error('User already exists'));
-  }
-
-  if (password !== verifyPassword) {
-    return Result.failure(new Error('Passwords do not match'));
   }
 
   const passwordHash = await bcrypt.hash(password, 10);
@@ -54,3 +44,54 @@ export async function registerUser({
 
   return Result.success(newUser);
 }
+
+// TODO: Repurpose to use for updating user password
+// /**
+//  * @summary Registers a new user
+//  * @param {object} params - The options to use.
+//  * @param {string} params.email - The user's email address.
+//  * @param {string} params.password - The user's password.
+//  * @param {string} params.verifyPassword - The user's verified password.
+//  * @param {PrismaClient} [params.client] - The Prisma client to use.
+//  * @returns {Promise<Result>} The result of the operation.
+//  */
+// export async function registerUser({
+//   email,
+//   password,
+//   verifyPassword,
+//   client = prismaClient,
+// }) {
+//   const user = await client.user.findUnique({
+//     where: {
+//       email,
+//     },
+//   });
+
+//   if (user !== null) {
+//     return Result.failure(new Error('User already exists'));
+//   }
+
+//   if (password !== verifyPassword) {
+//     return Result.failure(new Error('Passwords do not match'));
+//   }
+
+//   const passwordHash = await bcrypt.hash(password, 10);
+
+//   const newUser = await client.user.create({
+//     data: {
+//       email,
+//       passwordHash,
+//       userRoles: {
+//         create: {
+//           role: {
+//             connect: {
+//               name: 'user',
+//             },
+//           },
+//         },
+//       },
+//     },
+//   });
+
+//   return Result.success(newUser);
+// }

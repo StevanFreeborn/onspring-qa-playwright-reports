43d45f066ccc3a37db22b63edcc914f769a62458
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-11-17 13:17:12 -0600

    feat: add additional props to report

--- services/reports.js
+++ services/reports.js
@@ -12,18 +12,38 @@ import fs from 'fs';
  * @returns {Array<Report>} The test reports.
  */
 export function getReports() {
-  // report directory names have
-  // the format {date in ms}-{environment}-{status}
+  // report directory names contain the following pieces
+  // of information in order listed delimited by "-"
+  // + {date in ms}
+  // + {environment}
+  // + {status}
+  // + {github workflow name}
+  // + {github workflow run number}
+  // + {github workflow run attempt}
+  // + {pull request identifier} (only present if run on a pull request)
+  //   i.e. PR_{pull request number}
   // this is currently enforced by the test CI workflow
   // maybe we use database in future?
   const reportNames = fs.readdirSync('./reports');
   const reports = reportNames.map(reportName => {
-    const [date, environment, status] = reportName.split('-');
+    const [
+      date,
+      environment,
+      status,
+      workflowName,
+      runNumber,
+      runAttempt,
+      prId,
+    ] = reportName.split('-');
     return {
       date: parseInt(date),
       environment,
       status,
       path: reportName,
+      workflowName,
+      runNumber,
+      runAttempt,
+      prId,
     };
   });
 

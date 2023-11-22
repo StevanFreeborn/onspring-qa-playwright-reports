7ce5a0b6b7e4ee1c95d030ce142899f9d978490f
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-11-21 23:16:12 -0600

    tests: add test for login endpoints

--- tests/integration/auth.spec.js
+++ tests/integration/auth.spec.js
@@ -1,9 +1,10 @@
 import { JSDOM } from 'jsdom';
 import request from 'supertest';
 import { app } from '../../app.js';
+import { testUser } from './db.setup.js';
 
 describe('auth', () => {
-  describe('login', () => {
+  describe('GET /login', () => {
     test('it should return a 200 status code', async () => {
       const response = await request(app).get('/login');
 
@@ -38,6 +39,141 @@ describe('auth', () => {
         .getAttribute('content');
 
       expect(csrfToken).toBeDefined();
+    });
+  });
+
+  describe('POST /login', () => {
+    let csrfToken;
+    let cookies;
+
+    beforeAll(async () => {
+      const response = await request(app).get('/login');
+
+      const {
+        window: { document: view },
+      } = new JSDOM(response.text);
+
+      csrfToken = view
+        .querySelector('meta[name="csrf-token"]')
+        .getAttribute('content');
+
+      cookies = response.headers['set-cookie'];
+    });
+
+    test('it should return 500 error if no csrf tokens are present', async () => {
+      const response = await request(app)
+        .post('/login')
+        .type('x-www-form-urlencoded')
+        .send({
+          email: testUser.email,
+          password: testUser.password,
+        });
+
+      expect(response.statusCode).toBe(500);
+    });
+
+    test('it should return 500 error if no csrf cookie is present', async () => {
+      const response = await request(app)
+        .post('/login')
+        .type('x-www-form-urlencoded')
+        .send({
+          email: testUser.email,
+          password: testUser.password,
+          _csrf: csrfToken,
+        });
+
+      expect(response.statusCode).toBe(500);
+    });
+
+    test('it should return 500 error if no csrf token is present in request body', async () => {
+      const response = await request(app)
+        .post('/login')
+        .type('x-www-form-urlencoded')
+        .set('Cookie', cookies)
+        .send({
+          email: testUser.email,
+          password: testUser.password,
+        });
+
+      expect(response.statusCode).toBe(500);
+    });
+
+    test('it should return a 400 error if no password or email is provided', async () => {
+      const response = await request(app)
+        .post('/login')
+        .type('x-www-form-urlencoded')
+        .set('Cookie', cookies)
+        .send({
+          _csrf: csrfToken,
+        });
+
+      expect(response.statusCode).toBe(400);
+    });
+
+    test('it should return a 400 error if no password is provided', async () => {
+      const response = await request(app)
+        .post('/login')
+        .set('Cookie', cookies)
+        .type('x-www-form-urlencoded')
+        .send({ email: testUser.email, _csrf: csrfToken });
+
+      expect(response.statusCode).toBe(400);
+    });
+
+    test('it should return a 400 error if no email is provided', async () => {
+      const response = await request(app)
+        .post('/login')
+        .set('Cookie', cookies)
+        .type('x-www-form-urlencoded')
+        .send({ password: testUser.password, _csrf: csrfToken });
+
+      expect(response.statusCode).toBe(400);
+    });
+
+    test('it should return a 400 error if the email is not valid', async () => {
+      const response = await request(app)
+        .post('/login')
+        .set('Cookie', cookies)
+        .type('x-www-form-urlencoded')
+        .send({
+          email: 'not-an-email',
+          password: testUser.password,
+          _csrf: csrfToken,
+        });
+
+      expect(response.statusCode).toBe(400);
+    });
+
+    test('it should return a 400 error if the password is not valid', async () => {
+      const response = await request(app)
+        .post('/login')
+        .set('Cookie', cookies)
+        .type('x-www-form-urlencoded')
+        .send({
+          email: testUser.email,
+          password: 'short',
+          _csrf: csrfToken,
+        });
+
+      expect(response.statusCode).toBe(400);
+    });
+
+    test('it should return a 302 redirect to index view and set a new session cookie if the email and password are valid', async () => {
+      const response = await request(app)
+        .post('/login')
+        .set('Cookie', cookies)
+        .type('x-www-form-urlencoded')
+        .send({
+          email: testUser.email,
+          password: testUser.password,
+          _csrf: csrfToken,
+        });
+
+      expect(response.statusCode).toBe(302);
+      expect(response.headers.location).toBe('/');
+      expect(response.headers['set-cookie']).toEqual(
+        expect.arrayContaining([expect.stringMatching(/^connect.sid=.+/)])
+      );
     });
   });
 });
--- tests/integration/db.setup.js
+++ tests/integration/db.setup.js
@@ -1,28 +1,43 @@
 import { PrismaClient } from '@prisma/client';
 import { PostgreSqlContainer } from '@testcontainers/postgresql';
+import bcrypt from 'bcrypt';
 import { execSync } from 'child_process';
 
 let testContainer;
 let client;
 const timeout = 60000 * 10;
 
+const testPassword = 'password';
+const hashedTestPassword = bcrypt.hashSync(testPassword, 10);
+
+export const testAdminUser = {
+  email: 'admin@test.com',
+  password: testPassword,
+};
+
+export const testUser = {
+  email: 'user@test.com',
+  password: testPassword,
+};
+
 beforeAll(async () => {
   testContainer = await new PostgreSqlContainer().start();
   const connectionString = testContainer.getConnectionUri();
+
   process.env.DATABASE_URL = connectionString;
+
+  // want to instantiate a new client here so that we can use the
+  // DATABASE_URL environment variable that we have just overwritten
+  // with the test container connection string
+  client = new PrismaClient();
+
   execSync(
     `cross-env DATABASE_URL=${process.env.DATABASE_URL} prisma migrate dev`
   );
 }, timeout);
 
 beforeEach(async () => {
-  client = new PrismaClient();
-  await client.user.create({
-    data: {
-      email: `test@test.com`,
-      passwordHash: 'password',
-    },
-  });
+  await seedTestData(client);
 }, timeout);
 
 afterEach(async () => {
@@ -34,3 +49,62 @@ afterEach(async () => {
 afterAll(async () => {
   await testContainer.stop();
 }, timeout);
+
+/**
+ * @summary Seeds the test database with test data
+ * @returns {Promise<void>} A promise that resolves when the operation is complete
+ */
+async function seedTestData() {
+  await client.role.createMany({
+    data: [
+      {
+        name: 'admin',
+      },
+      {
+        name: 'user',
+      },
+    ],
+    skipDuplicates: true,
+  });
+
+  await client.user.create({
+    data: {
+      email: testAdminUser.email,
+      passwordHash: hashedTestPassword,
+      userRoles: {
+        create: [
+          {
+            role: {
+              connect: {
+                name: 'admin',
+              },
+            },
+          },
+          {
+            role: {
+              connect: {
+                name: 'user',
+              },
+            },
+          },
+        ],
+      },
+    },
+  });
+
+  await client.user.create({
+    data: {
+      email: testUser.email,
+      passwordHash: hashedTestPassword,
+      userRoles: {
+        create: {
+          role: {
+            connect: {
+              name: 'user',
+            },
+          },
+        },
+      },
+    },
+  });
+}
